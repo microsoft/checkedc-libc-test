1be84a1b808909f619399fb90743359dd4f06dd1
nsz <nsz@port70.net>
2011-08-05 00:56:27 +0200

    add remaining tests from libc-testsuit

---
+++ src/stdio/ungetc.c
@@ -0,0 +1,52 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <limits.h>
+#include <unistd.h>
+#include "test.h"
+
+#define TEST(r, f, x, m) ( \
+	errno = 0, ((r) = (f)) == (x) || \
+	(error("%s failed (" m ")\n", #f, r, x, strerror(errno)), 0) )
+
+#define TEST_S(s, x, m) ( \
+	!strcmp((s),(x)) || \
+	(error("[%s] != [%s] (%s)\n", s, x, m), 0) )
+
+void test_ungetc(void) {
+	int i;
+	char a[100];
+	FILE *f;
+
+	TEST(i, !(f = tmpfile()), 0, "failed to create temp file %d!=%d (%s)");
+
+	if (!f) return;
+
+	TEST(i, fprintf(f, "hello, world\n"), 13, "%d != %d (%m)");
+	TEST(i, fseek(f, 0, SEEK_SET), 0, "%d != %d (%m)");
+
+	TEST(i, feof(f), 0, "%d != %d");
+	TEST(i, fgetc(f), 'h', "'%c' != '%c'");
+	TEST(i, ftell(f), 1, "%d != %d");
+	TEST(i, ungetc('x', f), 'x', "%d != %d");
+	TEST(i, ftell(f), 0, "%d != %d");
+	TEST(i, fscanf(f, "%[h]", a), 0, "got %d fields, expected %d");
+	TEST(i, ftell(f), 0, "%d != %d");
+	TEST(i, fgetc(f), 'x', "'%c' != '%c'");
+	TEST(i, ftell(f), 1, "%d != %d");
+
+	TEST(i, fseek(f, 0, SEEK_SET), 0, "%d != %d");
+	TEST(i, ungetc('x', f), 'x', "%d != %d");
+	TEST(i, fread(a, 1, sizeof a, f), 14, "read %d, expected %d");
+	a[14] = 0;
+	TEST_S(a, "xhello, world\n", "mismatch reading ungot character");
+
+	TEST(i, fseek(f, 0, SEEK_SET), 0, "%d != %d");
+	TEST(i, fscanf(f, "%[x]", a), 0, "got %d fields, expected %d");
+	TEST(i, ungetc('x', f), 'x', "unget failed after fscanf: %d != %d");
+	TEST(i, fgetc(f), 'x', "'%c' != '%c'");
+	TEST(i, fgetc(f), 'h', "'%c' != '%c'");
+
+	fclose(f);
+}
