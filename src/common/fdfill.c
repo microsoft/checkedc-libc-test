2456ff49ea7ea4b5e88d70f967cf477d005519d8
Szabolcs Nagy <nsz@port70.net>
2013-07-20 21:05:07 +0000

    fix t_fdfill so it does not use much kernel side resources

--- src/common/fdfill.c
+++ src/common/fdfill.c
@@ -1,9 +1,15 @@
+#include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 #include "test.h"
 
 void t_fdfill(void)
 {
-	int fd[2];
-	while (pipe(fd) == 0);
-	dup(0);
+	int fd = 1;
+	if (dup(fd) == -1) {
+		if (errno == EMFILE)
+			return;
+		fd = open("/dev/null", O_RDONLY);
+	}
+	while(dup(fd) != -1);
 }
