0dee0dca0a980073abfcd4f78a328c77f1aa5979
Szabolcs Nagy <nsz@port70.net>
2013-08-06 00:30:10 +0000

    add pthread_condattr_setclock regression test

--- src/regression/mkdtemp-failure.c
+++ src/regression/mkdtemp-failure.c
@@ -1,5 +1,6 @@
 // commit: 69ecbd0f3188be97f91cc0d6415836d23e88f7fc 2011-02-19
 // commit: 382584724308442f03f3d29f7fc6de9e9d140982 2011-06-12
+// commit: c4685ae429a0cce4b285859d62a71fe603f0a864 2013-08-02
 // mkdtemp should return -1 on bad template
 #define _BSD_SOURCE 1
 #include <stdlib.h>
