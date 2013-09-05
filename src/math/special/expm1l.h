4f18b8f0b3cf4a59aea5258adcdd32ba2a0b6741
Szabolcs Nagy <nsz@port70.net>
2013-09-05 12:34:15 +0000

    math: add an expm1l special case

--- src/math/special/expm1l.h
+++ src/math/special/expm1l.h
@@ -5,3 +5,4 @@ T(RN,                       -0x1p+0L,      -0x1.43a54e4e988641cap-1L,   0x1.1484
 T(RN,                            inf,                            inf,          0x0p+0, 0)
 T(RN,                           -inf,                       -0x1p+0L,          0x0p+0, 0)
 T(RN,                            nan,                            nan,          0x0p+0, 0)
+T(RN,     -0x1.0000666666666666p+14L,                       -0x1p+0L,          0x0p+0, INEXACT)
