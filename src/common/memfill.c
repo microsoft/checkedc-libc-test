fc5db49aefdfc1f7eedf706e1b05c2af56732796
Szabolcs Nagy <nsz@port70.net>
2014-08-19 19:58:05 +0200

    fix memfill to use up libc reserved memory (glibc usually has some)

--- src/common/memfill.c
+++ src/common/memfill.c
@@ -6,11 +6,16 @@
 int t_memfill()
 {
 	int r = 0;
+	/* alloc mmap space with PROT_NONE */
 	if (t_vmfill(0,0,0) < 0) {
 		t_error("vmfill failed: %s\n", strerror(errno));
 		r = -1;
 	}
+	/* limit brk space */
 	if (t_setrlim(RLIMIT_DATA, 0) < 0)
 		r = -1;
+	if (!r)
+		/* use up libc reserves if any */
+		while (malloc(1));
 	return r;
 }
