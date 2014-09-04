d643f9eb7daeafe25ff81dcd2d4f239803823940
Szabolcs Nagy <nsz@port70.net>
2014-09-04 20:51:24 +0200

    separate dn_expand regression tests for empty name and pointer to 0

--- src/regression/dn_expand-empty.c
+++ src/regression/dn_expand-empty.c
@@ -5,8 +5,8 @@
 
 int main(void)
 {
-	unsigned char packet[3] = "";
-	char name[] = "x";
+	unsigned char packet[] = "";
+	char name[] = "XX";
 	int r;
 
 	/* empty name in packet */
@@ -15,15 +15,6 @@ int main(void)
 		t_error("dn_expand(\"\", name, 1) returned %d, wanted 1\n", r);
 	if (name[0])
 		t_error("dn_expand(\"\", name, 1) failed: got \"%s\" name, wanted \"\"\n", name);
-
-	/* empty name with offset pointer in packet */
-	memcpy(packet, "\xc0\2", 3);
-	name[0] = 'x';
-	r = dn_expand(packet, packet+3, packet, name, 1);
-	if (r!=2)
-		t_error("dn_expand(\"\\xc0\\2\", name, 1) returned %d, wanted 1\n", r);
-	if (name[0])
-		t_error("dn_expand(\"\\xc0\\2\", name, 1) failed: got \"%s\" name, wanted \"\"\n", name);
 
 	return t_status;
 }
