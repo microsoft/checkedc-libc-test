7d350259ac1468c9744839b0e6b78acb99dfd1c2
Szabolcs Nagy <nsz@port70.net>
2014-09-21 18:28:09 +0200

    lrand48 regression test

---
+++ src/regression/lrand48-signextend.c
@@ -0,0 +1,15 @@
+// lrand48 should give deterministic results
+#include <stdlib.h>
+#include "test.h"
+
+int main(void)
+{
+	long r;
+	r = lrand48();
+	if (r != 0) t_error("1st lrand48() got %ld want 0\n", r);
+	r = lrand48();
+	if (r != 2116118) t_error("2nd lrand48() got %ld want 2116118\n", r);
+	r = lrand48();
+	if (r != 89401895) t_error("3rd lrand48() got %ld want 89401895\n", r);
+	return t_status;
+}
