4be93bbd9546fb9f0155841935f0ed0d40f85a73
Szabolcs Nagy <nsz@port70.net>
2015-06-13 20:50:53 +0000

    setlocale C.UTF-8 in regex test that depends on UTF-8

--- src/regression/regex-escaped-high-byte.c
+++ src/regression/regex-escaped-high-byte.c
@@ -1,5 +1,6 @@
 // commit 39dfd58417ef642307d90306e1c7e50aaec5a35c 2015-03-20
 // backslash followed by high byte should be parsed as a normal sequence
+#include <locale.h>
 #include <regex.h>
 #include "test.h"
 
@@ -8,6 +9,8 @@ int main(void)
 	char buf[200];
 	regex_t r;
 	int n;
+
+	setlocale(LC_CTYPE, "C.UTF-8");
 
 	// illegal sequence (not U+00FC)
 	n = regcomp(&r, "\\\xfc", 0);
